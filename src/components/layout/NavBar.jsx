03d47a58e7c06e84ad610ad39b897b49ab641ebe
sm-remal <remal.mozzem@gmail.com>
2026-01-24 16:08:17 +0600

    update navbar

--- src/components/layout/NavBar.jsx
+++ src/components/layout/NavBar.jsx
@@ -1,53 +1,3 @@
-// "use client"
-// import React from 'react';
-// import Logo from './Logo';
-// import NavLink from '../button/NavLink';
-// import Link from 'next/link';
-// import { IoCart } from "react-icons/io5";
-
-// const NavBar = () => {
-//     const nav = <>
-//         <li><NavLink href={"/"}>Home</NavLink></li>
-//         <li><NavLink href={"/products"}>Products</NavLink></li>
-//         <li><NavLink href={"/blogs"}>Blogs</NavLink></li>
-//         <li><NavLink href={"/about"}>About Us</NavLink></li>
-//         <li><NavLink href={"/contact"}>Contact Us</NavLink></li>
-//     </>
-//     return (
-//         <div className='fixed top-0 right-0 left-0 z-50 bg-base-100'>
-//             <div className="navbar max-w-[1440px] mx-auto px-5">
-//                 <div className="navbar-start">
-//                     <div className="dropdown">
-//                         <div tabIndex={0} role="button" className="lg:hidden mr-2">
-//                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
-//                         </div>
-//                         <ul
-//                             tabIndex="-1"
-//                             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-//                             {nav}
-//                         </ul>
-//                     </div>
-//                     <Logo></Logo>
-//                 </div>
-//                 <div className="navbar-center hidden lg:flex">
-//                     <ul className="menu menu-horizontal px-1">
-//                         {nav}
-//                     </ul>
-//                 </div>
-//                 <div className="navbar-end gap-3">
-//                     <Link href={"/cart"} className='text-primary'>
-//                         <IoCart size={30} />
-//                     </Link>
-//                     <Link href={"/login"} className='btn btn-sm btn-primary btn-outline'>Login</Link>
-//                 </div>
-//             </div>
-//         </div>
-//     );
-// };
-
-// export default NavBar;
-
-
 "use client";
 import React from "react";
 import Logo from "./Logo";
@@ -107,9 +57,9 @@ const NavBar = () => {
           {/* --- Auth Section --- */}
           {session ? (
             <div className="dropdown dropdown-end">
-              <div tabIndex={0} className="btn btn-sm btn-primary btn-outline rounded-full flex items-center gap-2">
+              <div tabIndex={0} className="flex items-center gap-2">
                 {/* Show first letter of name as avatar */}
-                <span className="w-6 h-6 rounded-full bg-primary text-white flex items-center justify-center font-bold">
+                <span className="w-7 h-7 rounded-full bg-primary text-white flex items-center justify-center font-bold cursor-pointer">
                   {session.user?.name?.[0] || "U"}
                 </span>
               </div>
